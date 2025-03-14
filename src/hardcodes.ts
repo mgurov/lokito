3d0dcb688f43a3b46e9ef21fd0393859be4556da
Mykola Gurov <ngurov@gmail.com>
2025-03-14 13:19:59 +0100

    more custom trace ids

--- src/hardcodes.ts
+++ src/hardcodes.ts
@@ -1,4 +1,4 @@
 /** the idea of this file is to keep some things that should be configurable should the tool be used in any reasonably different context */
 
-export const TRACE_ID_FIELDS = ['mdc_traceId', 'x_b3_traceid'];
+export const TRACE_ID_FIELDS = ['mdc_traceId', 'x_b3_traceid', 'mdc_payload_data_traceId', 'mdc_traceid_backup', 'previous_traceid'];
 
