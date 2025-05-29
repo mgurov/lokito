f2b9c892604a6e5b8d4686b985c1952e1e482ee2
Mykola Gurov <ngurov@gmail.com>
2025-05-29 12:44:29 +0200

    clean

--- src/data/logData/logDataSlice.ts
+++ src/data/logData/logDataSlice.ts
@@ -65,7 +65,6 @@ export const logDataSlice = createSlice({
       const matcher = createFilterMatcher(filter);
       let matched = 0
       for (const line of state.logs) {
-        const {sourcesAndMessages, timestamp} = line
 
         const lineMatched = matcher.match({messages: line.sourcesAndMessages.map(sm => sm.message)});
 
