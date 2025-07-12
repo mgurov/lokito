9b41d3317028279dc1cbcc756fc9ba57ec1f4847
Mykola Gurov <ngurov@gmail.com>
2025-07-12 11:33:46 +0200

    fixed all fixed

--- src/routes/logs/acked/$sourceId.tsx
+++ src/routes/logs/acked/$sourceId.tsx
@@ -2,7 +2,7 @@ import LogsPage from "@/components/log/LogsPage";
 import { useParams } from "react-router-dom";
 
 export default function LogsRoute() {
-  const { sourceId } = useParams<{sourceId: string}>();
+  const { sourceId } = useParams<{ sourceId: string }>();
   return (
     <>
       <LogsPage sourceId={sourceId} ackNack="ack" />
