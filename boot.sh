76d3afc2c719dfc538488e90b00e5f6da156ae5f
Mykola Gurov <ngurov@gmail.com>
2025-11-08 08:23:26 +0100

    --no-save on install for the boot

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 set -x
-git fetch origin && git checkout ${1:-origin/main} && npm install && npm run build && npm run preview
+git fetch origin && git checkout ${1:-origin/main} && npm install --no-save && npm run build && npm run preview
